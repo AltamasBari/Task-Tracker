bccea7fce21816f0bc56d74c349c3431a9172cf8
AltamasBari <altamasbari@gmail.com>
2021-02-11 02:14:30 +0530

    db

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ function App() {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(
+      "https://my-json-server.typicode.com/altamasbari/task-tracker/tasks"
+    );
     const data = await res.json();
 
     return data;
@@ -26,7 +28,9 @@ function App() {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `https://my-json-server.typicode.com/altamasbari/task-tracker/tasks/${id}`
+    );
     const data = await res.json();
 
     return data;
@@ -34,13 +38,16 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
+    const res = await fetch(
+      "https://my-json-server.typicode.com/altamasbari/task-tracker/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     const data = await res.json();
 
@@ -49,9 +56,12 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
+    await fetch(
+      `https://my-json-server.typicode.com/altamasbari/task-tracker/tasks/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -61,13 +71,16 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
+    const res = await fetch(
+      `https://my-json-server.typicode.com/altamasbari/task-tracker/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      }
+    );
 
     const data = await res.json();
 
