6e2eddebe6713aa4f86dc1447ce72fd21259ad0d
AltamasBari <altamasbari@gmail.com>
2021-02-11 15:17:39 +0530

    local host

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch(
-      "https://my-json-server.typicode.com/altamasbari/db.json/tasks"
-    );
+    const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
 
     return data;
@@ -28,9 +26,7 @@ function App() {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(
-      `https://my-json-server.typicode.com/altamasbari/db.json/tasks/${id}`
-    );
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
 
     return data;
@@ -38,17 +34,13 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch(
-      "https://my-json-server.typicode.com/altamasbari/db.json/tasks",
-      {
-        method: "POST",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(task),
-      }
-    );
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
     const data = await res.json();
 
@@ -57,12 +49,9 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(
-      `https://my-json-server.typicode.com/altamasbari/db.json/tasks/${id}`,
-      {
-        method: "DELETE",
-      }
-    );
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -72,17 +61,13 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(
-      `https://my-json-server.typicode.com/altamasbari/db.json/tasks/${id}`,
-      {
-        method: "PUT",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(updTask),
-      }
-    );
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
 
     const data = await res.json();
 
